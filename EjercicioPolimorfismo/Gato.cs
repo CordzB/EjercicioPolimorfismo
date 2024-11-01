346217f38a0c1b1e1eb91c3ce9e9d96baa7af368
CordzB <167384843+CordzB@users.noreply.github.com>
2024-10-31 18:33:17 -0600

    Create Gato.cs

---
+++ EjercicioPolimorfismo/Gato.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace EjercicioPolimorfismo
+{
+    public class Gato : Animal
+    {
+        public override void HacerSonido()
+        {
+            Console.WriteLine("El gato maúlla.");
+        }
+
+        public override void Moverse()
+        {
+            Console.WriteLine("El gato salta.");
+        }
+
+    }
+}
