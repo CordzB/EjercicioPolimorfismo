79d8908254d99122696b9973fdb3fbd2e14a76b5
CordzB <167384843+CordzB@users.noreply.github.com>
2024-10-31 18:22:33 -0600

    Update Animal.cs

--- EjercicioPolimorfismo/Animal.cs
+++ EjercicioPolimorfismo/Animal.cs
@@ -4,6 +4,15 @@ namespace EjercicioPolimorfismo
 {
     public class Animal
     {
+        public virtual void HacerSonido()
+        {
+            Console.WriteLine("El animal hace un sonido.");
+        }
+
+        public virtual void Moverse()
+        {
+            Console.WriteLine("El animal se mueve de alguna forma.");
+        }
 
 
     }
