a3a425f5193b4bb936fb27711c8daf56085b538a
CordzB <167384843+CordzB@users.noreply.github.com>
2024-10-31 18:27:02 -0600

    Create Perro.cs

---
+++ EjercicioPolimorfismo/Perro.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace EjercicioPolimorfismo
+{
+    public class Perro : Animal
+    {
+        public override void HacerSonido()
+        {
+            Console.WriteLine("El perro ladra.");
+        }
+
+        public override void Moverse()
+        {
+            Console.WriteLine("El perro corre.");
+        }
+
+    }
+}
