7c37b0f4d6eac5c399667454f6179640860890c5
CordzB <167384843+CordzB@users.noreply.github.com>
2024-10-31 18:36:19 -0600

    Create Pajaro.cs

---
+++ EjercicioPolimorfismo/Pajaro.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace EjercicioPolimorfismo
+{
+    public class Pajaro : Animal
+    {
+        public override void HacerSonido()
+        {
+            Console.WriteLine("El pájaro canta.");
+        }
+
+        public override void Moverse()
+        {
+            Console.WriteLine("El pájaro vuela.");
+        }
+
+    }
+}
